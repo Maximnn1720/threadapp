a2e40f934ed153a1432579b2007dcef26e4124bd
Max Novikov <maximnn1720@gmail.com>
2022-04-22 13:35:39 +0300

    added threads writters application; added arrays of numbers generator; added logger

--- src/main/java/ru/study/application/AggregateFunctionsApp.java
+++ src/main/java/ru/study/application/AggregateFunctionsApp.java
@@ -2,6 +2,7 @@ package ru.study.application;
 
 import ru.study.application.AggregateFunctions.GetMaxFunction;
 import ru.study.application.AggregateFunctions.GetMinFunction;
+import ru.study.application.Utils.ArraysGenerator;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.concurrent.*;
 
 public class AggregateFunctionsApp {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(5, 8, 16, 85, 7, 3, 5);
+        List<Integer> numbers = ArraysGenerator.getListOfNumbers(5000, 10000);
         ExecutorService executor = Executors.newFixedThreadPool(2);
         List<Callable<Integer>> tasks = Arrays.asList(new GetMaxFunction(numbers), new GetMinFunction(numbers));
         CompletionService<Integer> completionService = new ExecutorCompletionService<>(executor);
